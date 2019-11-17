7d05fe9873c2b5c8529b4944e4392b62f097b308
André Martins <andrebertonhadev@gmail.com>
2019-11-17 09:59:42 -0300

    route validators

--- start/routes.js
+++ start/routes.js
@@ -3,18 +3,38 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('/files/:id', 'FileController.show')
 
 // rotas que serao chamadas somente se o usuario estiver logado
 Route.group(() => {
   Route.post('/files', 'FileController.store')
+
   // crud routes -> adonis route:list to see them
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map(
+      [
+        [
+          ['projects.store'],
+          ['Project']
+        ]
+      ]
+    ))
+
   // id do projeto
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map(
+      [
+        [
+          ['projects.tasks.store'],
+          ['Task']
+        ]
+      ]
+    ))
 }).middleware(['auth'])
