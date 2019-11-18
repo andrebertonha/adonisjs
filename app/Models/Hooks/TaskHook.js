891503411201e548d114a6368973689d41042b9c
André Martins <andrebertonhadev@gmail.com>
2019-11-18 04:21:41 -0300

    fila com redis no adonisjs

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
+
 const TaskHook = exports = module.exports = {}
 
 TaskHook.sendNewTaskMail = async taskInstance => {
@@ -12,22 +13,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
 
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message
-        .to(email)
-        .from('andrebertonhadev@gmail.com', 'André | AbmTech')
-        .subject('Nova tarefa para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
-
-  console.log('EXECUTOU')
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 })
 }
